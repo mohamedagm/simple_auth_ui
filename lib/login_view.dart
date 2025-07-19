1bf477e396e4afc24b639298da3e937564d52de7
mohamedagm <abogm44ud@gmail.com>
2025-07-19 19:51:45 +0200

    add gradiant to login_view

--- lib/login_view.dart
+++ lib/login_view.dart
@@ -23,129 +23,138 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Form(
-        key: formKey,
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Welcome Back',
-                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  'Enter your credintial to login',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-                ),
-                SizedBox(height: 20),
-                CustomTextField(
-                  hintText: 'Email',
-                  controller: emailController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'This field is required';
-                    } else if (!value.contains('@gmail')) {
-                      return 'دخل الايميل عدل يجدع انت';
-                    }
-                    return null;
-                  },
-                  picon: Icons.email,
-                ),
-                SizedBox(height: 10),
-                CustomTextField(
-                  hintText: 'Paswword',
-                  controller: passwordController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'This field is required';
-                    } else if (value.length <= 6) {
-                      return 'دي باسورد ترضي ربنا ؟';
-                    }
-                    return null;
-                  },
-                  picon: Icons.password,
-                  visible: visible,
-                  sicon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        visible = !visible;
-                        // visible ? visible = false : visible = true;
-                      });
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Color(0xFF9B28B2), Color(0xFFE3A7F4), Color(0xFFFCE4EC)],
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: formKey,
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Welcome Back',
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Enter your credintial to login',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                  ),
+                  SizedBox(height: 20),
+                  CustomTextField(
+                    hintText: 'Email',
+                    controller: emailController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'This field is required';
+                      } else if (!value.contains('@gmail')) {
+                        return 'دخل الايميل عدل يجدع انت';
+                      }
+                      return null;
                     },
-                    icon:
-                        visible
-                            ? Icon(Icons.visibility_off)
-                            : Icon(Icons.visibility),
+                    picon: Icons.email,
                   ),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xff9B28B2),
-                    minimumSize: Size(
-                      MediaQuery.of(context).size.width - 30,
-                      60,
+                  SizedBox(height: 10),
+                  CustomTextField(
+                    hintText: 'Paswword',
+                    controller: passwordController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'This field is required';
+                      } else if (value.length <= 6) {
+                        return 'دي باسورد ترضي ربنا ؟';
+                      }
+                      return null;
+                    },
+                    picon: Icons.password,
+                    visible: visible,
+                    sicon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          visible = !visible;
+                          // visible ? visible = false : visible = true;
+                        });
+                      },
+                      icon:
+                          visible
+                              ? Icon(Icons.visibility_off)
+                              : Icon(Icons.visibility),
                     ),
                   ),
-                  onPressed: () {
-                    if (formKey.currentState!.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text(
-                            'جدع يا حماده تم اصطياد الاكونت الخااص بك\n'
-                            'email: ${emailController.text}\n'
-                            'pass: ${passwordController.text}',
-                          ),
-                        ),
-                      );
-                      emailController.clear();
-                      passwordController.clear();
-                    }
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'forgot password ?',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
-                      color: Color(0xff9B28B2),
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Don't have an account?",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff9B28B2),
+                      minimumSize: Size(
+                        MediaQuery.of(context).size.width - 30,
+                        60,
                       ),
                     ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Sign Up',
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              'جدع يا حماده تم اصطياد الاكونت الخااص بك\n'
+                              'email: ${emailController.text}\n'
+                              'pass: ${passwordController.text}',
+                            ),
+                          ),
+                        );
+                        emailController.clear();
+                        passwordController.clear();
+                      }
+                    },
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'forgot password ?',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xff9B28B2),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
                         style: TextStyle(
                           fontSize: 18,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xff9B28B2),
+                          fontWeight: FontWeight.w400,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xff9B28B2),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
